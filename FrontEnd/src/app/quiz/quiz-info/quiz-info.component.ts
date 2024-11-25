157f88cdb4fe20e7b420f732c6d56409455512e8
Ronan Gomes <ronan_felipe@outlook.com>
2024-11-25 01:03:58 -0300

    Alterações que o Roni solicitou

--- FrontEnd/src/app/quiz/quiz-info/quiz-info.component.ts
+++ FrontEnd/src/app/quiz/quiz-info/quiz-info.component.ts
@@ -7,12 +7,12 @@ import { Component } from '@angular/core';
 })
 export class QuizInfoComponent {
   infos: string[]=[
-    'Quiz do Milhão é um criador de quiz, crie e jogue com seu quiz!',
+    'A Batalha do Saber é uma ferramenta criadora de quiz, crie e jogue com seu quiz!',
     'Você também pode buscar um quiz na sua lista. Além de poder editá-lo ou removê-lo',
     'Na criação, escolha se seu quiz terá 5 ou 10 questões. Cada questão terá no mínimo 2 e no máximo 4 alternativas.',
-    'E atenção na criação ed cada questão: A primeira resposta sempre será a correta, use os campos das demais respostas incorretas.',
+    'E atenção na criação de cada questão: A primeira resposta sempre será a correta, use os campos das demais respostas incorretas.',
     'Mas não se preocupe com a ordem" Ao jogar as alternativas virão aleatóriamente',
-    '-Hope you like it!',
+    '-Espero que goste!',
   ];
 
 
