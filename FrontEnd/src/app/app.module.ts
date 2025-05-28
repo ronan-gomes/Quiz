afebd6c0dab5dc3f2c9b75536228891d90b9288b
Ronan Gomes <ronan_felipe@outlook.com>
2025-05-28 19:51:54 -0300

    atualizado para angular v18

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { QuizInfoComponent } from './quiz/quiz-info/quiz-info.component';
 import { BtnHomeComponent } from './components/btn-home/btn-home.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CommonModule } from '@angular/common'
 import { QuizListComponent } from './quiz/quiz-list/quiz-list.component';
 import { QuizFormComponent } from './quiz/quiz-form/quiz-form.component';
@@ -18,29 +18,22 @@ import { QuizScreenComponent } from './quiz/quiz-screen/quiz-screen.component';
 import { QuizDeleteComponent } from './quiz/quiz-delete/quiz-delete.component';
 
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    QuizInfoComponent,
-    BtnHomeComponent,
-    QuizListComponent,
-    QuizFormComponent,
-    AlertComponent,
-    QuizScreenComponent,
-    QuizDeleteComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    HttpClientModule,
-    CommonModule,
-    ReactiveFormsModule,
-    FormsModule,
-    ModalModule.forRoot()
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HomeComponent,
+        QuizInfoComponent,
+        BtnHomeComponent,
+        QuizListComponent,
+        QuizFormComponent,
+        AlertComponent,
+        QuizScreenComponent,
+        QuizDeleteComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        NgbModule,
+        CommonModule,
+        ReactiveFormsModule,
+        FormsModule,
+        ModalModule.forRoot()], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
